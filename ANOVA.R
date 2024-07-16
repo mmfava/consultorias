2d6a5110311c10437a8837108ed127ec01939f14
Marília Melo Favalesso <mariliabioufpr@gmail.com>
2024-07-15 22:37:01 -0300

    Update ANOVA.R

--- ANOVA.R
+++ ANOVA.R
@@ -1,27 +1,26 @@
 ##
-##  ANOVA (An?lise de Vari?ncia)
-##  
-## --------------------------------------
-
-## A an?lise de vari?ncia ? um teste de hipoteses para modelos lineares
-
-## Em um modelo, constru?mos uma express?o na qual elementos (vari?veis
-## e constantes) determinam as causas de varia??o de uma vari?vel.
-
-## PA <-- sexo + idade + etnia + sedentarismo + elast?ncia dos vasos + ignorados
-
-## A hip?tese nula em um modelo de an?lise de vari?ncia e dada por:
-## H0: "A m?dia de cada vari?vel tratamento ? igual as demais"
-
-## Hipotese alternativa ser? a seguinte:
-## Ha: "Pelo menos uma das medias eh diferente das demais"
-
-## Assim, a ANOVA compara a varia??o devida aos tratamentos com
-## a varia??o devido ao res?duo, por isso resulta em um teste
-## F-Snedecor, semelhante a compara??o de vari?ncias. O teste
-## ainda requer que as vari?veis dos tratamentos tenham distri-
-## bui??o normal e que as vari?ncias sejam homog?neas.
-
+## ANOVA (Análise de Variância)
+## Marília Melo Favalesso - mariliabioufpr@gmail.com
+## ------------------------------------------------------
+## A análise de variância é um teste de hipóteses para modelos lineares.
+##
+## Em um modelo, construímos uma expressão na qual elementos (variáveis
+## e constantes) determinam as causas de variação de uma variável.
+##
+## PA <-- sexo + idade + etnia + sedentarismo + elasticidade dos vasos + ignorados
+##
+## A hipótese nula em um modelo de análise de variância é dada por:
+## H0: "A média de cada variável tratamento é igual às demais".
+##
+## A hipótese alternativa será a seguinte:
+## Ha: "Pelo menos uma das médias é diferente das demais".
+##
+## Assim, a ANOVA compara a variação devida aos tratamentos com
+## a variação devida ao resíduo, por isso resulta em um teste
+## F-Snedecor, semelhante a comparação de variâncias. O teste
+## ainda requer que as variáveis dos tratamentos tenham distribuição
+## normal e que as variâncias sejam homogêneas.
+##
 ## ANOVA
 ## -----------------------------------------------------
 ## F.V.          G.L.   S.Q.      Q.M          F.
@@ -30,7 +29,7 @@
 ## Desconhecido   n-k   SQd   QMd=SQd/(n-k)
 ## -----------------------------------------------------
 ## Total          n-1
-# ------------------------------------------------------
+## ------------------------------------------------------
 
 ##########################################################################################################
 
@@ -39,10 +38,9 @@
 ## Entrada de dados
 ## --------------------------------------------------------
 exercicio = data.frame(
-  treatment = rep(1:3,each=6),
-  response = c(14,14,13,12,15,13,24,25,19,22,18,22,
-               20,22,22,16,15,16))
-
+  treatment = rep(1:3, each = 6),
+  response = c(14, 14, 13, 12, 15, 13, 24, 25, 19, 22, 18, 22,
+               20, 22, 22, 16, 15, 16))
 
 ## Escrevendo o modelo
 exercicio.model = lm(formula = response ~ treatment, 
@@ -50,85 +48,79 @@ exercicio.model = lm(formula = response ~ treatment,
 
 exercicio.model
 
-## Visualizacao grafica dos tratamentos
-boxplot(formula=response~treatment,data=exercicio)
+## Visualização gráfica dos tratamentos
+boxplot(formula = response ~ treatment, data = exercicio)
 
-## Valida??o do modelo
+## Validação do modelo
 ##
-## A especifica??o do modelo estabelece que os res?duos
-## (erro ou termo desconhecido) tenham vari?ncias 
-## constantes segundo os tratamentos e distribui??o 
-## normal. A valida??o do modelo ? essencial para se 
-## proceder a an?lise.
+## A especificação do modelo estabelece que os resíduos
+## (erro ou termo desconhecido) tenham variâncias 
+## constantes segundo os tratamentos e distribuição 
+## normal. A validação do modelo é essencial para se 
+## proceder à análise.
 ##
-## Com o modelo ajustado (coeficientes) pode ser utilizado
-## para realizar predi??es, assim, os valores preditos pelo\
-## modelo sao dados por
+## Com o modelo ajustado (coeficientes) pode-se realizar
+## predições. Assim, os valores preditos pelo modelo são dados por:
 
 preditos = fitted(object = exercicio.model)
 preditos
 
-## Os res?duos (erros) s?o obtidos pela difenca entre
-## o valor obtido e o valor predito (estimado) pelo modelo
+## Os resíduos (erros) são obtidos pela diferença entre
+## o valor observado e o valor predito (estimado) pelo modelo:
 
 residuos = resid(object = exercicio.model)
 residuos
 
-## ? importanmte fazer um  gr?fico que ilustre a dispers?o 
-## entre os valores reais (medidos) e os valores preditos 
-## (estimados). Numa situa??o ideal, a quantidade de pontes 
-## deve ser uniformemente distribuidos nos quadrantes formados 
-## pela m?dias, ou seja, a dispers?o n?o pode sugerir algum 
-## tipo de padrao ou de associa??o.
+## É importante fazer um gráfico que ilustre a dispersão 
+## entre os valores reais (observados) e os valores preditos 
+## (estimados). Em uma situação ideal, os pontos devem ser
+## uniformemente distribuídos ao redor da linha de média zero,
+## o que sugere homogeneidade das variâncias:
 
 plot(x = preditos, y = residuos,
      pch = 20,
      col = "black")
 
-abline(h=mean(residuos), col = "azure4")
+abline(h = mean(residuos), col = "azure4")
 abline(v = mean(preditos))
 
-## A aus?ncia de padr?es sugere a homogeneidade das vari?ncias
+## A ausência de padrões sugere homogeneidade das variâncias.
 
-## Teste de homogeneidade de vari?ncias segundo grupos
+## Teste de homogeneidade de variâncias entre grupos
 ## (Teste de Levene)
 
 library(car)
 
-## O teste de Levene testa se todas as vari?ncias s?o
-## homog?neas
+## O teste de Levene testa se todas as variâncias são
+## homogêneas.
 
-## Ho: Todas as vari?ncias s?o iguais
-## Ha: Pelo menos uma das vari?ncias diverge das demais
+## Ho: Todas as variâncias são iguais.
+## Ha: Pelo menos uma das variâncias diverge das demais.
 
-## Se p-valor for menor do que 0.05 (5%) o teste
-## sera significativo (Rejeita-se Ho)
+## Se o p-valor for menor do que 0.05 (5%), o teste
+## é significativo (rejeita-se Ho).
 
 leveneTest(residuos,
-           group=as.factor(exercicio$treatment))
+           group = as.factor(exercicio$treatment))
 
 ## Como p-valor = 0.02938 foi menor do que 0.05, o teste
-## foi significativo para a heterogeneidade (se rejeita
+## foi significativo para a heterogeneidade (rejeita-se
 ## Ho).
-##
 
-## Teste para a normalidade dos residuos
+## Teste para a normalidade dos resíduos
 shapiro.test(residuos)
 
-## Como p-valor foi maior do que 0.05 entao os residuos
-##  tem distribuicao normal, logo a ANOVA poderia
-## ser empregada, caso o primeiro pressuporto de 
-## homogeneidade das vari?ncias fosse aceito!!!
+## Como o p-valor foi maior do que 0.05, os resíduos
+## têm distribuição normal. Logo, a ANOVA pode ser
+## empregada, caso o primeiro pressuposto de 
+## homogeneidade das variâncias seja aceito!!!
 
-
-## Ent?o, quais s?o os crit?rios a serem avaliados?
-## - Homogeneidade da vari?ncia dos res?duos (Teste de Levene)
-## - Distribui??o normal dos res?duos (shapiro-wilk)
+## Então, quais são os critérios a serem avaliados?
+## - Homogeneidade da variância dos resíduos (Teste de Levene).
+## - Distribuição normal dos resíduos (Shapiro-Wilk).
 
 ## No nosso caso, o que podemos concluir?
 
-## Mas caso fosse poss?vel, como far?amos a ANOVA?
+## Mas caso fosse possível, como faríamos a ANOVA?
 
 anova(exercicio.model)
-
-
